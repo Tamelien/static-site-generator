db6b304e4be4853f1a075c2d328f25d55b974aa7
Tamelien <Tamelien@protonmail.com>
2025-12-22 18:33:43 +0100

    Add copy files function

---
+++ src/copystatic.py
@@ -0,0 +1,18 @@
+import os
+import shutil
+
+
+def copy_files(source_dir_path, target_dir_path):
+    if not os.path.exists(target_dir_path):
+        print(f" make dir {target_dir_path}")
+        os.mkdir(target_dir_path)
+
+    for path in os.listdir(source_dir_path):
+        source_path = os.path.join(source_dir_path, path)
+        target_path = os.path.join(target_dir_path, path)
+        
+        if os.path.isdir(source_path):
+            copy_files(source_path, target_path)
+        else:
+            print(f" copy {source_path} -> {target_path}")
+            shutil.copy(source_path, target_path)
--- src/main.py
+++ src/main.py
@@ -1,37 +1,16 @@
-from textnode import (TextNode,
-                      TextType)
-
-from markdown_to_html_node import markdown_to_html_node
-from textnode import text_node_to_html_node
-
+import shutil
+import os
+from copystatic import copy_files
 
 def main():
 
-    md = """
-This is **bolded** paragraph
-text in a p
-tag here
+    target_path = "./public/"    
+    source_path = "./static/"
 
-This is another paragraph with _italic_ text and `code` here
-
-"""
-
-    node = markdown_to_html_node(md)
-    html = node.to_html()
-    print(html)
-    
-
-    md = """
-```
-This is text that _should_ remain
-the **same** even with inline stuff
-```
-"""
-
-    node = markdown_to_html_node(md)
-    html = node.to_html()
-
-    print(html)
+    if os.path.exists(target_path):
+        shutil.rmtree(target_path)   
+            
+    copy_files(source_path, target_path)
 
 if __name__ == "__main__":
     main()
